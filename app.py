e8b4e91b967f74eef0e4880d6426f2d57d6ad849
joaovitor-a1 <jvitor1450@gmail.com>
2025-12-08 23:32:30 -0300

    Estrutura do Sistema: Criação de APP.PY

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "chave_segura"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tarefas.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.permanent_session_lifetime = timedelta(minutes=40)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from routes import tarefas_bp
+    app.register_blueprint(tarefas_bp)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
